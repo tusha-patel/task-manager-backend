4151e3e4982386bfc6d5b306ab51ae65d90fd970
tusha-patel <tusha.b@queueloopsolutions.com>
2025-06-27 17:52:24 +0530

    small changes

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 const ConnectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL, {});
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("mongodb connected  ");
     } catch (error) {
         console.log("db connect error", error);
